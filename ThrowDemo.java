b9253d50983b92d84a99d4981fa39767528f293a
dsp115 <dpinale1@my.epcc.edu>
2026-09-14 15:38:24 +0000

    exceptions with catch

---
+++ ThrowDemo.java
@@ -0,0 +1,30 @@
+public class ThrowDemo {
+    public static int slicesPerPerson(int slices, int people)
+    {
+        System.out.println("Inside sclicesPerPerson");
+
+        if(people <= 0) {
+            throw new IllegalArgumentException("Number of people must be positive");
+        }
+        return slices / people;
+    }
+    public static void showShare(int slices, int people) {
+        System.out.println("Starting calculation");
+
+        int share = slicesPerPerson(slices, people);
+        
+
+        System.out.println("Slices per person: " + share);
+        System.out.println("Calculation finished");
+    }
+
+    public static void main(String[] args) {
+        try {
+            showShare(12, 0);                                                                           // Inputs
+            System.out.println("Return normally to main");
+        } catch (IllegalArgumentException e){
+            System.out.println("Cannot calculate: " + e.getMessage());
+        }
+        System.out.println("Program continues");
+    }
+}
